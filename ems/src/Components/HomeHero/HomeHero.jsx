33f253ac3b440ff07f1ba6ec8cfebab6a268e043
Dinusha_Madhuranga <dinushamadhuranga77@gmail.com>
2024-07-29 01:52:47 +0530

    day 1

---
+++ ems/src/Components/HomeHero/HomeHero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeHero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-central">
+      <img src={logo} alt="" />
+      </div>
+      
+    </div>
+  )
+}
+
+export default HomeHero
